4b3b18fc3d6c10e6515ff06f95f79dc81bbd2599
Tom <emailfortomg@gmail.com>
2020-01-25 10:28:55 +0000

    Moved to self-contained engine model

--- Readme.md
+++ Readme.md
@@ -5,36 +5,7 @@
 ![powered by javascript badge](https://img.shields.io/badge/powered%20by-javascript-red)
 [![Maritime Development Badge](https://img.shields.io/badge/engine%20for-maritime-brightgreen)](https://github.com/t0mgithub/maritime)
 
-Routing engine to be used with Maritime to enable Express style routing. Maritime has been designed to allow different routing engines to be used to compile routes to regexp, to allow users to choose and change the syntax of the routing. A routing engine must contain `toRegex()` to convert a standard path to a regexp and a list of keys and `createParams()` to create an object of URL parameters from keys, original route, requested path and matches from `regexp.match()`.
-
-### Usage
-
-#### toRegex()
-
-toRegex is used to convert a standard express route such as `/:username/*` to a regexp, to be matched in the Maritime router.
-
-```js
-const { toRegex } = require("maritime-standard-routing");
-toRegex("/:username", []);
-// returns:
-// {
-//   regex: /^\/(?:([^\/]+?))\/?$/i,
-//   keys: [ { name: 'username', optional: false, offset: 1 } ]
-// }
-```
-
-#### createParams()
-
-createParams is used to create a list of URL parameters based on request path and keys - different engines can use data such as path, keys and match data from the regexp.
-
-```js
-const { createParams } = require("maritime-standard-routing");
-createParams({
-  path: "/hello",
-  keys: [{ name: "username", optional: false, offset: 1 }]
-});
-// returns: { username: 'hello' }
-```
+Routing engine to be used with Maritime to enable Express style routing. Maritime has been created to allow different routing engines to be used for different routing syntaxs. An engine must handle creating regexp, matching routes and creating route parameters. All extra data that could be used later (such as route keys or the regexp match data) should be stored on the engine object.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,47 +1,38 @@
 const pathToRegexp = require("path-to-regexp");
 
-/**
- * Convert standard route to regexp and compile list of keys.
- *
- * @param {String} [path] Base routing path to build regexp for.
- * @param {Array} [keys] List of keys to pass to path-to-regexp.
- * @param {Object} [options] Options to apply to path-to-regexp.
- *
- * @return {Object} Object containing created regex and route keys array.
- */
-module.exports.toRegex = function(path, keys = [], options) {
-  let regex = pathToRegexp(path, keys, options);
-  return {
-    regex,
-    keys
-  };
-};
-
-/**
- * Create parameters from route data.
- *
- * @param {Object} [data] Object containing data which can be used to create route parameters.
- * @param {String} [data.match] Array of matches returned by regex.exec().
- * @param {Array} [data.keys] Array of keys from routes.
- *
- * @return {Object} Object containing all route params.
- */
-module.exports.createParams = function(data) {
-  let { match, keys } = data;
-  params = {};
-
-  for (let i = 1; i < match.length; i++) {
-    let key = keys[i - 1];
-    let prop = key.name;
-    let val = decodeURIComponent(match[i]);
-
-    if (
-      val !== undefined ||
-      !Object.prototype.hasOwnProperty.call(params, prop)
-    ) {
-      params[prop] = val;
-    }
+module.exports = class RoutingEngine {
+  constructor(options) {
+    this.options = options || {};
   }
 
-  return params;
+  createRegex(route) {
+    this.keys = [];
+    let regex = pathToRegexp(route, this.keys, this.options);
+    this.regex = regex;
+    return regex;
+  }
+
+  match(path) {
+    this.lastMatch = this.regex.exec(path);
+    return this.lastMatch ? true : false;
+  }
+
+  createParams(path) {
+    let params = {};
+
+    for (let i = 1; i < this.lastMatch.length; i++) {
+      let key = this.keys[i - 1];
+      let prop = key.name;
+      let val = decodeURIComponent(this.lastMatch[i]);
+
+      if (
+        val !== undefined ||
+        !Object.prototype.hasOwnProperty.call(params, prop)
+      ) {
+        params[prop] = val;
+      }
+    }
+
+    return params;
+  }
 };
